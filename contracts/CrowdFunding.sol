9dab837f095da044c8a61e6c86cfc31df6c7efd7
humayun-ahmad <humayunraj789@gmail.com>
2024-02-13 03:03:10 +0600

    Implemented refund function

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -14,8 +14,24 @@ contract CrowdFunding{
 
     constructor(uint _target, uint _deadline){
         target = _target;
-        deadline = _deadline;
+        deadline = block.timestamp + _deadline;
         minimumContribution=100 wei;
         manager=msg.sender;
+    }
+
+    function sendEth() public payable {
+        require(block.timestamp < deadline, "Deadline has passed!");
+        require(msg.value >= minimumContribution, "Minimum Contibution is not met");
+        if(contributors[msg.sender] == 0){
+            noOfContributors++;
+        }
+
+        contributors[msg.sender]+=msg.value;
+        raisedAmount+= msg.value;
+
+    }
+
+    function getContractBalance() public view returns(uint){
+        return address(this).balance;
     }
 }
